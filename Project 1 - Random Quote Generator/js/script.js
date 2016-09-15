8d055888e438ed0903dd849203a2b15cf651b2eb
randynj <rsingh1415@gmail.com>
2016-09-15 15:37:32 -0400

    Test - creating new branch in GitHub
    First comment

--- Project 1 - Random Quote Generator/js/script.js
+++ Project 1 - Random Quote Generator/js/script.js
@@ -1,3 +1,5 @@
+//This is my edit for testing of creating a new branch in GitHub
+
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 //document.getElementById('loadQuote').addEventListener("click", printQuote, false);
